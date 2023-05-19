c4727378afc23c5ca564f2a4d3d9c4d552dfde05
alnty <132160193+alnty@users.noreply.github.com>
2023-05-20 00:33:14 +0300

    hm3

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-class Human:
-    def __init__(self, name="Student"):
+class Animal:
+    def __init__(self, name="Animal"):
         self.name = name
-class Supplies:
-    def __int__(self, books=None, pens=None):
-        self.books = books
-        self.pens = pens
-    def get_books(self):
-        print("I'm going to take book from the library.")
-        self.books += 1
-    def get_pens(self):
-        print("I'm going to buy new pen in the store.")
-        self.pens += 1
-    def live(self, week):
-        day = str(week) + " week" + " of " + "the " + self.name + "'s" ' life.'
-        print(f'{day:=^35}')
-        live_cube = random.randint(1, 2)
-        if live_cube == 1:
-            self.get_books()
-        elif live_cube == 2:
-            self.get_pens()
+class Zoo:
+    def __init__(self, species):
+        self.species = []
+    def add_kind(self, animal):
+        self.species.append(animal)
+    def print_animal_species(self):
+        if self.species != []:
+            print(f"Types of animals in Zoo: ")
+            for species in self.species:
+                print(species.name)
+        else:
+            print(f"There are no animals.")
+cat = Animal("Cat")
+dog = Animal("Dog")
+lizard = Animal("Lizard")
+rabbit = Animal("Rabbit")
+hamster = Animal("Hamster")
+zoo = Zoo("Zoo")
+
+zoo.add_kind(cat)
+zoo.add_kind(dog)
+zoo.add_kind(lizard)
+zoo.add_kind(rabbit)
+zoo.add_kind(hamster)
+zoo.print_animal_species()
