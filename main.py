012543d82d84cde3917d5225f0408d257fbe7a75
alnty <132160193+alnty@users.noreply.github.com>
2023-05-19 22:52:01 +0300

    testing

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+class Human:
+    def __init__(self, name="Student"):
+        self.name = name
 class Supplies:
-    def __init__(self, name="Preschooler" books=None, pens=None, copybooks=None):
+    def __int__(self, books=None, pens=None):
+        self.books = books
+        self.pens = pens
+    def get_books(self):
+        print("I'm going to take book from the library.")
+        self.books += 1
+    def get_pens(self):
+        print("I'm going to buy new pen in the store.")
+        self.pens += 1
+    def live(self, week):
+        day = str(week) + " week" + " of " + "the " + self.name + "'s" ' life.'
+        print(f'{day:=^35}')
+        live_cube = random.randint(1, 2)
+        if live_cube == 1:
+            self.get_books()
+        elif live_cube == 2:
+            self.get_pens()
