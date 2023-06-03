307ed27324359970a89ff14ec1f0ea125ff035a5
alnty <132160193+alnty@users.noreply.github.com>
2023-06-03 18:55:39 +0300

    cat_with_glasses

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
-import urllib.request
-import requests
+import cv2
+from PIL import Image
 
-#opener = urllib.request.build_opener()
+image_path = "cat.jpeg"
+cat_face_cascade = cv2.CascadeClassifier("haarcascade_frontalcatface_extended.xml")
+image = cv2.imread(image_path)
 
-#response = opener.open("https://httpbin.org/get")
+cat_face = cat_face_cascade.detectMultiScale(image)
 
-#print(response.read())
+cat = Image.open(image_path)
+glasses = Image.open("glasses.png")
+cat = cat.convert("RGBA")
+glasses = glasses.convert("RGBA")
 
-
-#res = requests.post("https://httpbin.org/post", data="Test data hello world", headers={"h1": "My title"})
-
-
-#response = requests.get("https://httpbin.org/get")
-#print(response.content)
-#print(f"Datatype ", {response.text})
-
-response = requests.get("https://coinmarketcap.com/")
-response_text = response.text
-
-coin_list = []
-
-response_parse = response_text.split("<span>")
-for parse_elem1 in response_parse:
-    if parse_elem1.startswith("$"):
-        for parse_elem2 in parse_elem1.split("</span>"):
-            if parse_elem2.startswith("$") and parse_elem2[1].isdigit():
-                coin_list.append(parse_elem2)
-
-btc = coin_list[0]
-print("BTC =", btc)
+for (x,y,w,h) in cat_face:
+    glasses = glasses.resize((w, int(h/3)))
+    cat.paste(glasses, (x, int(y + h/4)), glasses)
+    cat.save("cat_with_glasses.png")
+    cat_with_glasses = cv2.imread("cat_with_glasses.png")
+    cv2.imshow("Cat_with_glasses.png", cat_with_glasses)
+    cv2.waitKey()
